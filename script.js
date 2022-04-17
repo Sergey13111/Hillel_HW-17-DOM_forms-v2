e7457ef5501769c1e6c06d5e92d2dfd4aea052db
Sergey13111 <boiko.sergey93@gmail.com>
2022-04-17 12:34:08 +0300

    оптимизация кода и доработка обработчика для вычеркивания задач

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+"use strict";
+
 function createNode(element) {
     return document.createElement(element);
-}
+};
 
 function append(parent, el) {
     return parent.append(el);
-}
+};
 
 function prepend(parent, el) {
     return parent.prepend(el);
+};
+
+function removeTask(event) {
+    const isRemoveButton = event.target.nodeName === "BUTTON";
+
+    if (isRemoveButton) {
+        const messageBlock = event.target.closest(".text-message");
+        return messageBlock.remove();
+    } 
+};
+
+function removeError() {
+    const isErrorField = message.classList.contains("error");
+
+    if (isErrorField) {
+        message.classList.remove("error");
+        errorMessage.classList.remove("alert");
+        errorMessage.innerHTML = "";
+    }
+    return;
 }
 
 const form_input = document.forms.form_message;
 const {message} = form_input;
 const errorMessage = document.querySelector(".error-message");
 const ul = document.getElementById("list");
+
+message.onfocus = removeError;
+ul.addEventListener("click", (removeTask));
 
 form_input.onsubmit = (event) => {
     event.preventDefault();
@@ -27,18 +52,19 @@ form_input.onsubmit = (event) => {
 
     const li = createNode("li");
     const checkbox = createNode("input");
+    const span = createNode("span")
     const button = createNode("button");
 
-    
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("name", "completed");
     checkbox.setAttribute("class", "form-check-input")
     li.classList.add("text-message");
-    li.innerHTML = message.value;
+    span.innerHTML = message.value;
     button.textContent = "Delete";
 
 
     append(ul, li);
+    append(li, span)
     prepend(li, checkbox);
     append(li, button);
     form_input.reset();
@@ -48,33 +74,18 @@ form_input.onsubmit = (event) => {
         const isChecked = event.target.className === "form-check-input";
 
         if (isChecked) {
-            const messageBlock = event.target.closest(".text-message");
             const chbox = event.target;
-     
-            messageBlock.classList.add("line-through");
+            span.classList.add("line-through");
             chbox.disabled = true;
             btnRemove.disabled = true;
         };
     });
 };
 
-message.onfocus = () => {
-    const isErrorField = message.classList.contains("error");
-  
-    if (isErrorField) {
-        message.classList.remove("error");
-        errorMessage.classList.remove("alert");
-        errorMessage.innerHTML = "";
-    }
-}
 
-ul.addEventListener("click", (event) => {
-    const isRemoveButton = event.target.nodeName === "BUTTON";
 
-    if (isRemoveButton) {
-        const messageBlock = event.target.closest(".text-message");
-        messageBlock.remove();
-    } 
-});
+
+
+
 
 
